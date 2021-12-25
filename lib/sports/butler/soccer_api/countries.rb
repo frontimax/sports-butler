80dc8f41f1f0a9b8ca645875ac9b49de7453a3ae
Joerg Kirschstein <info@joerg-kirschstein.de>
2021-12-25 12:25:04 +0100

    main hotifx travis ci failing

--- lib/sports/butler/soccer_api/countries.rb
+++ lib/sports/butler/soccer_api/countries.rb
@@ -1,17 +1,17 @@
-# # frozen_string_literal: true
-# module Sports
-#   module Butler
-#     module SoccerApi
-#       class Countries < Sports::Butler::SoccerApi::Base
-#         def available_endpoint_methods
-#           [:all, :by_name, :by_id]
-#           # search_by_name, by_code
-#         end
-#
-#         def path
-#           :countries
-#         end
-#       end
-#     end
-#   end
-# end
+# frozen_string_literal: true
+module Sports
+  module Butler
+    module SoccerApi
+      class Countries < Sports::Butler::SoccerApi::Base
+        def available_endpoint_methods
+          [:all, :by_name, :by_id]
+          # search_by_name, by_code
+        end
+
+        def path
+          :countries
+        end
+      end
+    end
+  end
+end
--- lib/sports/butler/soccer_api/head_to_head.rb
+++ lib/sports/butler/soccer_api/head_to_head.rb
@@ -1,16 +1,16 @@
-# frozen_string_literal: true
-module Sports
-  module Butler
-    module SoccerApi
-      class HeadToHead < Sports::Butler::SoccerApi::Base
-        def available_endpoint_methods
-          [:by_match, :by_teams]
-        end
-
-        def path
-          :matches
-        end
-      end
-    end
-  end
-end
+# # frozen_string_literal: true
+# module Sports
+#   module Butler
+#     module SoccerApi
+#       class HeadToHead < Sports::Butler::SoccerApi::Base
+#         def available_endpoint_methods
+#           [:by_match, :by_teams]
+#         end
+#
+#         def path
+#           :matches
+#         end
+#       end
+#     end
+#   end
+# end
